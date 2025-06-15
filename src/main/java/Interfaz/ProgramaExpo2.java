51a322002e1a9250af7b8d3b2f159e9374dd60c5
FernandoT230 <110936728+FernandoT230@users.noreply.github.com>
2025-06-14 20:02:35 -0700

    Add files via upload

---
+++ src/main/java/Interfaz/ProgramaExpo2.java
@@ -0,0 +1,20 @@
+package Interfaz;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author Chapita
+ */
+
+
+
+public class ProgramaExpo2 {
+
+    public static void main(String[] args) {
+        Login objetoLogin = new Login();
+        objetoLogin.setVisible(true);
+    }
+}
