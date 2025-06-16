ebeccfdf7dd5db9d59e52f4d4f8382ab28acc4e1
FernandoT230 <110936728+FernandoT230@users.noreply.github.com>
2025-06-15 23:36:57 -0400

    Update CConexion.java

--- src/main/java/conexion/CConexion.java
+++ src/main/java/conexion/CConexion.java
@@ -6,25 +6,17 @@ import javax.swing.JOptionPane;
 
 // Clase para gestionar la conexión a la base de datos MySQL
 public class CConexion {
-    // Objeto de conexión
     Connection conectar;
-    // Usuario de la base de datos
     String usuario = "root";
-    // Contraseña de la base de datos
     String contrasenia = "";
-    // Nombre de la base de datos
     String bd = "cafeina";
-    // IP del servidor de base de datos
     String ip = "localhost";
-    // Puerto de conexión
     String puerto = "3306";
-    // Cadena de conexión JDBC
     String cadena = "jdbc:mysql://"+ip+":"+puerto+"/"+bd;
 
-    // Método para establecer la conexión con la base de datos
+    
     public Connection estableceConexion(){
         try {
-            // Carga el driver de MySQL y realiza la conexión
             Class.forName("com.mysql.jdbc.Driver");
             conectar = DriverManager.getConnection(cadena,usuario,contrasenia);
         } catch (Exception e){
