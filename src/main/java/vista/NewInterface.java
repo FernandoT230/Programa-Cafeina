51ebb5abdb0af4e9c448da30d314441badaa58d0
FernandoT230 <110936728+FernandoT230@users.noreply.github.com>
2025-06-15 23:16:16 -0400

    Delete src directory

--- src/main/java/vista/NewInterface.java
+++
@@ -1,5 +0,0 @@
-package vista;
-
-public class NewInterface {
-    // Implementa aquí la nueva interfaz si es necesario
-}
--- src/main/java/vista/ProgramaExpo2.java
+++
@@ -1,8 +0,0 @@
-package vista;
-
-public class ProgramaExpo2 {
-    public static void main(String[] args) {
-        Login login = new Login(); 
-        login.setVisible(true);
-    }
-}
